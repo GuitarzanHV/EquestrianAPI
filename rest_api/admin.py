f4a0ee12f8290090f5229a22930c4b68d0d87742
Hunter VanHorn <guitarhunter92@msn.com>
2015-09-28 23:34:40 -0400

    Completed Questionnaire Display Models, registered them with django admin

--- rest_api/admin.py
+++ rest_api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Questionnaire
+from .models import Category
+from .models import Question
+from .models import Answer
 
 # Register your models here.
+admin.site.register(Questionnaire)
+admin.site.register(Category)
+admin.site.register(Question)
+admin.site.register(Answer)
--- rest_api/models.py
+++ rest_api/models.py
@@ -1,23 +1,50 @@
 from django.db import models
 
 # Create your models here.
+class Questionnaire(models.Model):
+    """Lists the categories for each questionnaire type, as
+    well as the maximum composite score for each score type
+    """
+    questionnaire_name = models.CharField(max_length=255)
+    acceptable_score = models.IntegerField(default=0)
+    needs_work_score = models.IntegerField(default=0)
+    unacceptable_score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.questionnaire_name
+
+class Category(models.Model):
+    """Holds the questions for each scored category, as well
+    as maximum score for each score type. Many to Many 
+    relationships with Question and Questionnaire.
+    """
+    category_name = models.CharField(max_length=255)
+    acceptable_score = models.IntegerField(default=0)
+    needs_work_score = models.IntegerField(default=0)
+    unacceptable_score = models.IntegerField(default=0)
+    questionnaires = models.ManyToManyField(Questionnaire)
+
+    def __str__(self):
+        return self.category_name
+
 class Question(models.Model):
-    """docstring for Question"""
+    """Holds the text of a question, along with an image
+    that goes with the question. Multiple Answers for each
+    Question, Questions can be in more than one Category
+    """
     question_text = models.CharField(max_length=255)
     question_image = models.CharField(max_length=255)
-    categories = models.ManyToMany(Category)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.question_text
 
 class Answer(models.Model):
-    """docstring for Answer"""
+    """Text and numeric score for an Answer to a 
+    Question. Each Answer only has one Question."""
     answer_text = models.CharField(max_length=255)
     score = models.IntegerField(default=0)
     question = models.ForeignKey(Question)
 
-class Category(models.Model):
-    """docstring for Category"""
-    category_name = models.CharField(max_length=255)
-    questionnaires = models.ManyToMany(Questionnaire)
-
-class Questionnaire(models.Model):
-    """docstring for Questionnaire"""
-    questionnaire_name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.answer_text
