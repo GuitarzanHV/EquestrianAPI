c13c1b86a33a0996ae3479b934b621bf1212a279
Hunter VanHorn <guitarhunter92@msn.com>
2015-10-02 00:32:07 -0400

    Update .gitignore, don't need migration files upstreamed

--- rest_api/models.py
+++ rest_api/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Questionnaire(models.Model):
     """Lists the categories for each questionnaire type, as
     well as the maximum composite score for each score type
