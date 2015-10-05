a16487e33e3c3e7d8f22fd83cbff620be8c5325f
Hunter VanHorn <guitarhunter92@msn.com>
2015-10-05 18:48:27 -0400

    Imported AnswerGroup into admin.py

--- rest_api/admin.py
+++ rest_api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Questionnaire, Category, Question, Answer
+from .models import Questionnaire, Category, Question, AnswerGroup, Answer
 
 # Register your models here.
 admin.site.register(Questionnaire)
