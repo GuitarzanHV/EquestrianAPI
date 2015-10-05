3d057075a64449916bfb3b3cacdc20324eaac76b
Hunter VanHorn <hvanhor@bgsu.edu>
2015-10-05 17:37:40 -0400

    I want to merge from upstream, git

--- EquestrianAPI/wsgi.py
+++ EquestrianAPI/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EquestrianAPI.settings")
+sys.path.append('/var/www/django/django/Equestrian_API')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EquestrianAPI.production_settings")
 
 application = get_wsgi_application()
